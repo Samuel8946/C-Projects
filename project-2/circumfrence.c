a169b57957986f8163f1867c7d09be04ff7aa567
Samuel8946 <samuelcharles18isme@gmail.com>
2023-07-03 08:57:39 -0400

    here we go for now

--- project-2/circumfrence.c
+++ project-2/circumfrence.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+int calc_circ(int userInput);
 int main(void)
 {
+    // Introduction to program
     printf("Welcome to my second project where it calculates the circumference of a circle! =)\n");
     printf("Please report and bugs later on I will try my best to fix them.\n\n");
+
+    // Prompt user for the radius
+    int radius;
+    printf("Please enter the radius value: ");
+    scanf("%d", &radius);
+    
+
+    // Calculate circumference of the circle
+    // calc_circ(radius);
 }
